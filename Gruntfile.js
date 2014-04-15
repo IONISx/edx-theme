28da0e002f9bdef671b5a31a1dd99e1531b4f442
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-04-15 20:42:33 +0200

    Start adding less and grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    var config = {
+        'static': 'static',
+        bower: 'bower_components'
+    };
+
+    grunt.initConfig({
+        c: config,
+
+        // ## //
+
+        watch: {
+            less: {
+                files: ['<%= c.static %>/less/**/*.less'],
+                tasks: ['less', 'autoprefixer']
+            }
+        },
+
+        // ## //
+
+        jshint: {
+            options: {
+                'curly': true,
+                'eqeqeq': true,
+                'immed': true,
+                'latedef': true,
+                'newcap': true,
+                'noarg': true,
+                'noempty': true,
+                'unused': true,
+                'undef': true,
+                'trailing': true,
+                'quotmark': 'single',
+
+                'boss': true,
+                'eqnull': true
+            },
+            browser: {
+                files: {
+                    src: [
+                    ]
+                },
+                options: {
+                    'browser': true,
+                    'jquery': true,
+                    'globals': {
+                        'smoothScroll': true
+                    }
+                }
+            }
+        },
+
+        // ## //
+
+        less: {
+            theme: {
+                options: {
+                    paths: ['<%= c.bower %>']
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= c.static %>/less',
+                    src: 'main.less',
+                    dest: '<%= c.static %>/css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        // ## //
+
+        autoprefixer: {
+            template: {
+                files: {
+                    '<%= c.static %>/css/main.css': [
+                        '<%= c.static %>/css/main.css'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', function () {
+        grunt.option('force', true);
+
+        grunt.task.run([
+            'less',
+            'autoprefixer',
+            'watch'
+        ]);
+    });
+};
